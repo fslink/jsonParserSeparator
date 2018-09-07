f82b6ddc1ef90faf3a449c4c63361429b325a04d
FS_link <mowglysofiann@gmail.com>
2018-09-07 16:52:41 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+})
+
+.get('/exe', function(req, res, next) {
+
+    fs.readFile(path.join('D:', 'spotinfo', 'T_ent.json'), {'encoding': 'utf-8'}, (err, data) => {
+      if (err) throw err;
+      var re = /},/gi;
+      var nouvChn = data.replace(re, '}');
+      fs.appendFile(path.join('D:', 'spotinfo', 'new_T_ent.json'), nouvChn, function (err) {
+          if (err) throw err;
+          console.log('Saved!');
+      }); 
+
+    });
+
+    res.send('OK');
+})
+
+module.exports = router;
